a7cd5fc34e3870a7455d529a60002df6ea8e9ed0
Egor Ivanov <dia.tarasevich@gmail.com>
2024-03-11 01:19:32 +0300

    update tasks

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,9 +1,11 @@
 import {
-	Controller, Get
+	Body,
+	Controller, Get, HttpCode, Param, Post, Put, UsePipes, ValidationPipe
 } from '@nestjs/common'
 import { TaskService } from './task.service'
 import { Auth } from '../auth/decorators/auth.decorator'
 import { CurrentUser } from '../auth/decorators/user.decorator'
+import { TaskDto } from './task.dto'
 
 
 @Controller('user/tasks')
@@ -15,5 +17,27 @@ export class TaskController {
 	async getAll(@CurrentUser('id') userId:string){
 		return this.taskService.getAll(userId)
 	}
+
+
+@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Post()
+	@Auth()
+	async create(@Body() dto: TaskDto, @CurrentUser('id') userId: string){
+		return this.taskService.create(dto, userId)
+}
+
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Put(':id')
+	@Auth()
+	async update(@Body() dto: TaskDto,
+							 @CurrentUser('id') userId: string,
+							 @Param('id') id: string
+							 ){
+		return this.taskService.update(dto, id, userId)
+	}
+
 
 }
